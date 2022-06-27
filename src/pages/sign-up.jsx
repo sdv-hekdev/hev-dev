264fe1d3620fdabc981a52c1c5443ab4512b6474
evanlv <evanlvdev@gmail.com>
2022-06-27 23:37:54 +0200

    chore: clean code

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -44,7 +44,7 @@ const SignUpPage = (props) => {
         <div className="w-full max-w-md space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-black">
-              Sign to your account
+              Create my account
             </h2>
           </div>
 
